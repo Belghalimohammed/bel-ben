7d4b76b3402a752c3b58a9220e5ed49e3bed3ec2
Belghalimohammed <Belghalimohammed0@gmail.com>
2021-02-28 22:16:29 +0000

    index

---
+++ views/content.php
@@ -0,0 +1,260 @@
+<div class="container-xxl" >
+  <div class="row" style="width : 100%; ">
+    <div class="col-md-3" style=" ">
+     <div class="text-center" style="width : 100%; height : 143px ; border : 2px solid black;">
+
+
+     <div class="card" style="width: 100%;">
+  <div class="card-header">
+    Quote
+  </div>
+  <div class="card-body">
+    
+      <p>A well-known quote, contained in a blockquote element.</p>
+   
+  </div>
+
+</div>
+
+     </div> </br>
+     <div class="text-center" style="width : 100%; height : 600px; border : 2px solid black;">
+   
+     <div class="container">
+     <br>
+  <div class="row">
+    <div class="col-sm" style="border : 1px solid black;">
+     <p>1</p>
+    </div>
+    <div class="col-sm offset-1" style="border : 1px solid black;">
+    <p>2</p>
+    </div>
+  </div> 
+</div>
+
+     </div>
+    </div>
+
+
+
+
+<div class="col-lg-7" style=" height : 100% ;">
+    <div class="text-center" style="width : 100%; height : 100% ; border : 2px solid black;">
+    
+
+    <div class="card" style="width: 100%;height : 300px;">
+        <div class="card-header">
+            Quote
+        </div>
+        <div class="card-body">
+            
+            <p>A well-known quote, contained in a blockquote element.</p>
+        
+        </div>
+
+    </div> <br>
+
+<div class="card" style="width: 100%;height : 300px;">
+  <div class="card-header">
+    Quote
+  </div>
+  <div class="card-body">
+    
+      <p>A well-known quote, contained in a blockquote element.</p>
+   
+  </div>
+
+</div> <br>
+<div class="card" style="width: 100%;height : 300px;">
+  <div class="card-header">
+    Quote
+  </div>
+  <div class="card-body">
+    
+      <p>A well-known quote, contained in a blockquote element.</p>
+   
+  </div>
+
+</div> <br>
+<div class="card" style="width: 100%;height : 300px;">
+  <div class="card-header">
+    Quote
+  </div>
+  <div class="card-body">
+    
+      <p>A well-known quote, contained in a blockquote element.</p>
+   
+  </div>
+
+</div> <br>
+<div class="card" style="width: 100%;height : 300px;">
+  <div class="card-header">
+    Quote
+  </div>
+  <div class="card-body">
+    
+      <p>A well-known quote, contained in a blockquote element.</p>
+   
+  </div>
+
+</div> <br>
+<div class="card" style="width: 100%;height : 300px;">
+  <div class="card-header">
+    Quote
+  </div>
+  <div class="card-body">
+    
+      <p>A well-known quote, contained in a blockquote element.</p>
+   
+  </div>
+
+</div> <br>
+<div class="card" style="width: 100%;height : 300px;">
+  <div class="card-header">
+    Quote
+  </div>
+  <div class="card-body">
+    
+      <p>A well-known quote, contained in a blockquote element.</p>
+   
+  </div>
+
+</div> <br>
+<div class="card" style="width: 100%;height : 300px;">
+  <div class="card-header">
+    Quote
+  </div>
+  <div class="card-body">
+    
+      <p>A well-known quote, contained in a blockquote element.</p>
+   
+  </div>
+
+</div> <br>
+<div class="card" style="width: 100%;height : 300px;">
+  <div class="card-header">
+    Quote
+  </div>
+  <div class="card-body">
+    
+      <p>A well-known quote, contained in a blockquote element.</p>
+   
+  </div>
+
+</div> <br>
+<div class="card" style="width: 100%;height : 300px;">
+  <div class="card-header">
+    Quote
+  </div>
+  <div class="card-body">
+    
+      <p>A well-known quote, contained in a blockquote element.</p>
+   
+  </div>
+
+</div> <br>
+<div class="card" style="width: 100%;height : 300px;">
+  <div class="card-header">
+    Quote
+  </div>
+  <div class="card-body">
+    
+      <p>A well-known quote, contained in a blockquote element.</p>
+   
+  </div>
+
+</div> <br>
+<div class="card" style="width: 100%;height : 300px;">
+  <div class="card-header">
+    Quote
+  </div>
+  <div class="card-body">
+    
+      <p>A well-known quote, contained in a blockquote element.</p>
+   
+  </div>
+
+</div> <br>
+<div class="card" style="width: 100%;height : 300px;">
+  <div class="card-header">
+    Quote
+  </div>
+  <div class="card-body">
+    
+      <p>A well-known quote, contained in a blockquote element.</p>
+   
+  </div>
+
+</div> <br>
+<div class="card" style="width: 100%;height : 300px;">
+  <div class="card-header">
+    Quote
+  </div>
+  <div class="card-body">
+    
+      <p>A well-known quote, contained in a blockquote element.</p>
+   
+  </div>
+
+</div> <br>
+<div class="card" style="width: 100%;height : 300px;">
+  <div class="card-header">
+    Quote
+  </div>
+  <div class="card-body">
+    
+      <p>A well-known quote, contained in a blockquote element.</p>
+   
+  </div>
+
+</div> <br>
+<div class="card" style="width: 100%;height : 300px;">
+  <div class="card-header">
+    Quote
+  </div>
+  <div class="card-body">
+    
+      <p>A well-known quote, contained in a blockquote element.</p>
+   
+  </div>
+
+</div> <br>
+<div class="card" style="width: 100%;height : 300px;">
+  <div class="card-header">
+    Quote
+  </div>
+  <div class="card-body">
+    
+      <p>A well-known quote, contained in a blockquote element.</p>
+   
+  </div>
+
+</div> <br>
+
+
+   </div>
+    </div>
+
+
+
+
+    <div class="col col-lg-2 " style=" height : 250px ;">
+    <div class="text-center" style="width : 100%; height : 100% ; border : 2px solid red;">
+
+    <div class="card" style="width: 100%;">
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">An item</li>
+    <li class="list-group-item">A second item</li>
+    <li class="list-group-item">A third item</li>
+    <li class="list-group-item">An item</li>
+    <li class="list-group-item">A second item</li>
+    <li class="list-group-item">A third item</li>
+  </ul>
+</div>
+
+
+   </div>
+    </div>
+
+
+  </div>
+</div>
---
+++ views/header.php
@@ -0,0 +1,20 @@
+
+<nav class="navbar navbar-light bg-light fixed-top" style="height : 60px;">
+  <div class="container-fluid">
+  <form class="d-flex" >
+    <div style="margin-right : 400px;">
+  <button class="btn btn-primary" type="button" style="margin-right : 50px;">Sign In</button>
+  <button class="btn btn-success" type="button" >Register</button>
+</div>
+  <form class="text-center" style="">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" style="width : 580px ; height : 35px;">
+      
+    </form>
+    
+    <a class="navbar-brand" style="margin-bottom : 15px;">MorBest</a>
+    </form>
+ 
+   
+  </div>
+</nav>
+</br></br></br></br></br></br>
